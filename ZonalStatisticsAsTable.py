44c21551ef20950b6282d26a8083abffac3119f6
Tao Huang <huangt@caryinstitute.org>
2022-04-29 17:45:28 -0600

    Add files via upload

--- ZonalStatisticsAsTable.py
+++ ZonalStatisticsAsTable.py
@@ -1,7 +1,8 @@
 import arcpy
+import os
 from arcpy.sa import *
 for yr in xrange(2010,2021+1):
     for day in xrange(yr*10000+801, yr*10000+830+1):
         input=os.path.join( r"C:\Users\taohuang\Downloads\prism-main",str(yr),  "PRISM_tmax_stable_4kmD2_"+str(day)+"_bil.bil")
-        output=os.path.join( r"C:\Users\taohuang\Downloads\prism-main",str(yr),  "PRISM_tmax_stable_4kmD2_"+str(day)+".dbf")
+        output=os.path.join( r"C:\Users\taohuang\Downloads\prism-main","output", "PRISM_tmax_stable_4kmD2_"+str(day)+".dbf")
         ZonalStatisticsAsTable("USGS West Mnts gages","GAGE_ID",input,output,"DATA","ALL")
