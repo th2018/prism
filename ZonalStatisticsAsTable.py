b1268a5cc64040444aa8e2717bcffcd843a9d646
Tao Huang <th2018@wildcats.unh.edu>
2022-12-26 13:26:39 -0700

    Add files via upload

--- ZonalStatisticsAsTable.py
+++ ZonalStatisticsAsTable.py
@@ -2,10 +2,14 @@ import arcpy
 import os
 from arcpy.sa import *
 folder=r'C:\Users\taohuang\Downloads\prism-main20221226\prism-main'
-for yr in xrange(2012,2022+1):
+for yr in xrange(2013,2022+1):
     for day in xrange(yr*10000+701, yr*10000+930+1):
 #        input=os.path.join( r"C:\Users\taohuang\Downloads\prism-main",str(yr),  "PRISM_tmax_stable_4kmD2_"+str(day)+"_bil.bil")
         input=os.path.join( folder,str(yr),  "PRISM_tmax_stable_4kmD2_"+str(day)+"_bil.bil")
         output=os.path.join( folder,"output", "PRISM_tmax_stable_4kmD2_"+str(day)+".dbf")
 #        output=os.path.join( r"C:\Users\taohuang\Downloads\prism-main","output", "PRISM_tmax_stable_4kmD2_"+str(day)+".dbf")
         ZonalStatisticsAsTable("bas_nonref_WestXeric","GAGE_ID",input,output,"DATA","ALL")
+        vinput=os.path.join(  folder,str(yr),  "PRISM_vpdmax_stable_4kmD2_"+str(day)+"_bil.bil")
+        voutput=os.path.join( folder,"output", "PRISM_vpdmax_stable_4kmD2_"+str(day)+".dbf")
+        ZonalStatisticsAsTable("bas_nonref_WestXeric","GAGE_ID",input,output,"DATA","ALL")
+        
---
+++ ZonalStatisticsAsTable_VPD.py
@@ -0,0 +1,10 @@
+import arcpy
+import os
+from arcpy.sa import *
+folder=r'C:\Users\taohuang\Downloads\prism-main20221226\prism-main'
+for yr in xrange(2013,2022+1):
+    for day in xrange(yr*10000+701, yr*10000+929+1):
+        vinput=os.path.join(  folder,str(yr),  "PRISM_vpdmax_stable_4kmD2_"+str(day)+"_bil.bil")
+        voutput=os.path.join( folder,"output", "PRISM_vpdmax_stable_4kmD2_"+str(day)+".dbf")
+        ZonalStatisticsAsTable("bas_nonref_WestXeric","GAGE_ID",vinput,voutput,"DATA","ALL")
+        
