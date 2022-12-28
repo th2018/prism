cf0bd66c2ff07b58ccd5a34534b77b00ab201565
Tao Huang <th2018@wildcats.unh.edu>
2022-12-28 13:24:55 -0700

    Add files via upload

--- ZonalStatisticsAsTable_VPD.py
+++ ZonalStatisticsAsTable_VPD.py
@@ -1,8 +1,10 @@
 import arcpy
 import os
 from arcpy.sa import *
+from itertools import chain
+
 folder=r'C:\Users\taohuang\Downloads\prism-main20221226\prism-main'
-for yr in xrange(2013,2022+1):
+for yr in xrange(2012,2022+1):
     for day in xrange(yr*10000+701, yr*10000+929+1):
         vinput=os.path.join(  folder,str(yr),  "PRISM_vpdmax_stable_4kmD2_"+str(day)+"_bil.bil")
         voutput=os.path.join( folder,"output", "PRISM_vpdmax_stable_4kmD2_"+str(day)+".dbf")
