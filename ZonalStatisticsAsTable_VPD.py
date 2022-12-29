ef11f1871804c1b3a8ae89e482a3f7b28cd6c90a
Tao Huang <th2018@wildcats.unh.edu>
2022-12-29 14:07:04 -0700

    Add files via upload

--- ZonalStatisticsAsTable_VPD.py
+++ ZonalStatisticsAsTable_VPD.py
@@ -5,9 +5,11 @@ from itertools import chain
 
 folder=r'C:\Users\taohuang\Downloads\prism-main20221226\prism-main'
 
-res = chain(range(20140701,20140731+1),range(20140801,20140831+1) )
+yr=2015
+
+res = chain(range( yr*10000 + 701, yr*10000 + 731+1),range( yr*10000 + 801, yr*10000 + 831+1) )
     
-for yr in xrange(2014,2022+1):
+for yr in range( yr ,2022+1):
     for day in res:
         vinput=os.path.join(  folder,str(yr),  "PRISM_vpdmax_stable_4kmD2_"+str(day)+"_bil.bil")
         voutput=os.path.join( folder,"output", "PRISM_vpdmax_stable_4kmD2_"+str(day)+".dbf")
